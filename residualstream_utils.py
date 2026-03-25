3ff48e0066f560faa39c81cda0ae57748da8441a
Thomas <hitchon.thomas@gmail.com>
2026-03-25 13:15:24 +0000

    Modif : utiliser generate pour recup les hidden states

--- residualstream_utils.py
+++ residualstream_utils.py
@@ -46,27 +46,38 @@ def tokenize_prompt(prompt_user, tokenizer, model, is_chat=False, prompt_system=
 
     return inputs
 
-# Does a forwardpass and return the hidden states of the model
-def get_hidden_states(model, inputs):
-    with torch.no_grad():
-        outputs = model(
-            **inputs,
-            output_hidden_states=True,
-        )   
-    return outputs.hidden_states
+def get_hidden_states(model, tokenizer, inputs):
+    model_outputs = model.generate(**inputs, 
+                                   return_dict_in_generate=True, 
+                                   output_hidden_states=True,
+                                   max_new_tokens=5) 
+    
+    generated_ids = model_outputs.sequences[0][len(inputs.input_ids[0]) :]
+    prediction = tokenizer.decode(generated_ids, skip_special_tokens=True)
 
-# Prints the top-k tokens from each hidden layers, decoded using model.lm_head (norm?)
-def print_top_tokens_per_layer(prompt, expected_next_token, model, tokenizer, is_already_tokenized=False, start_layer=30, end_layer=36, top_k=20):
+    print(f"Model output : {prediction}")
+    return model_outputs.hidden_states[0]
+
+def print_top_tokens_per_layer(prompt, expected_next_token, model, tokenizer, is_already_tokenized=False, apply_last_norm=False, start_layer=30, end_layer=36, top_k=20):
     if(not is_already_tokenized):
-        inputs = tokenize_prompt(prompt, tokenizer, model)
+        inputs = tokenize_prompt(prompt, tokenizer, model)#, is_chat=True, prompt_system="The first token you generate should be the answer to the question.")
     else:
         inputs = prompt
-    hidden_states = get_hidden_states(model, inputs)
+    hidden_states = get_hidden_states(model, tokenizer, inputs)
     lm_head = model.lm_head
- 
+    #print("Norme moyenne des embeddings du residual stream")
     for i, h in enumerate(hidden_states[start_layer:end_layer+1], start=start_layer):
         last_token_hidden = h[:, -1, :]  
-        #last_token_hidden = model.model.norm(last_token_hidden)
+
+        if(apply_last_norm):
+            before_norm = last_token_hidden.norm().mean()
+            last_token_hidden = model.model.norm(last_token_hidden)
+            after_norm = last_token_hidden.norm().mean()
+            #print(f"layer {i}: [avant normalisation : {before_norm:.2f}, après normalisation: {after_norm:.2f}]")
+        
+        # if(i == end_layer and not apply_last_norm):
+        #     print(f"i == {i}")
+        #     last_token_hidden = model.model.norm(last_token_hidden)
 
         logits = lm_head(last_token_hidden)
         probs = torch.softmax(logits, dim=-1)
@@ -76,7 +87,7 @@ def print_top_tokens_per_layer(prompt, expected_next_token, model, tokenizer, is
         for j, tid in enumerate(top_token_ids[0]):
             decoded_token = tokenizer.decode([tid.item()])
             if(tid.item() == tokenizer.encode(expected_next_token)[0] or remove_spaces(decoded_token) == remove_spaces(expected_next_token)):
-                print(f"\n /!\\ ALERT : {expected_next_token} detected -> top {j+1} /!\\ \n")
+                print(f"    -> {expected_next_token} detected -> top {j+1}")
             decoded_tokens.append(decoded_token)
 
         probs_list = [p.item() for p in top_probs[0]]
